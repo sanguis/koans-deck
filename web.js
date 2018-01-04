f75406a853173f3d6cc576f7387b92637534ce7b
Beauregard, Josh <sanguis@users.noreply.github.com>
2018-01-04 14:21:49 -0500

    getting ready for heroku

--- web.js
+++ web.js
@@ -1,6 +1,8 @@
 var express = require("express");
+var morgan = require('morgan')
+
 var app = express();
-app.use(express.logger());
+app.use(morgan('combined'));
 app.use("/", express.static(__dirname));
 
 var port = process.env.PORT || 5000;
